55bea69056c1fcf3728a190b41f0c5fce53daca7
Jasiel <jasiel.quezada@unl.edu.ec>
2024-02-02 12:24:24 -0500

    registro

--- Sistema/views.py
+++ Sistema/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def registro(request):
+    if request.method == 'GET':
+        return render(request, 'registro.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('')
+            except IntegrityError:
+                return render(request, 'registro.html', {'form': UserCreationForm,
+                                              "error": 'Username ya existe'})
+        return render(request, 'registro.html', {'form': UserCreationForm,
+                                                 "error": 'Contraseña no coincide'})
+
